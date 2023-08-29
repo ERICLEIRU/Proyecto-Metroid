354c73dc7012d6f3825cf9fad89fafcbba3efad2
ERICLEIRU <143230608+ERICLEIRU@users.noreply.github.com>
2023-08-29 01:52:14 +0000

    se agrego iostream

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
+#include <iostream>
+
 int main(int argc, char const *argv[])
 {
+    std::cout << "Mensaje" << std::endl;
+
     for (int nombre = 0; nombre < 10; nombre++)
     {
         /* code */
     }
-    
+
     /* code */
     return 0;
 }
